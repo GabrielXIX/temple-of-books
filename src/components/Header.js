623c37d225f897c4a2e307218d2ccf5f5ac762a2
Gabriel Dueñas Nuñez <65438145+GabrielXIX@users.noreply.github.com>
2023-05-09 02:18:52 -0700

    Delete src directory

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import logo from '../logo_tecnm_itt.jpg';
-
-export default function Header() {
-    return (
-        <div style={{padding: '1rem', textAlign: 'center'}}>
-            <img style={{height: '100px'}} src={logo} alt='Logo TecNM ITT' />
-        </div>
-    )
-}
